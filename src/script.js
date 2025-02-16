f644a9e13ce89e9b4abd3f8c599bb6af93aaad16
Thales Alves <thalesalves758@gmail.com>
2025-02-15 21:56:13 -0300

    implementa quantidade de estrelas dinamico

--- src/script.js
+++ src/script.js
@@ -75,42 +75,67 @@ function renderProducts(products) {
       const cents = product.price - value * 100;
 
       return `
-      <div class="flex w-full" id="product-${product.id}">
-        <div class="flex w-full lg:flex-col lg:items-center border border-gray-200 rounded-sm">
-          <div class="flex justify-center items-center w-4/10 lg:w-2/3">
-            <img class="object-scale-down size-26 lg:size-38 xl:size-50" src="./images/${product.pictures?.[0]}" alt="${product.name}">
-          </div>
+        <div class="flex w-full" id="product-${product.id}">
+          <div class="flex w-full lg:flex-col lg:items-center border border-gray-200 rounded-sm">
+            <div class="flex justify-center items-center w-4/10 lg:w-2/3">
+              <img class="object-scale-down size-26 lg:size-38 xl:size-50" src="./images/${product.pictures?.[0]}" alt="${product.name}">
+            </div>
 
-          <div class="flex flex-col grow w-6/10 items-start py-3 px-2 justify-center border-l border-l-gray-100 lg:w-full lg:border-l-0 lg:border-t lg:border-t-gray-100">
-            <p class="font-bold flex items-start w-full overflow-hidden text-ellipsis h-[50px]">
-              ${product.name}
-            </p>
+            <div class="flex flex-col grow w-6/10 items-start py-3 px-2 justify-center border-l border-l-gray-100 lg:w-full lg:border-l-0 lg:border-t lg:border-t-gray-100">
+              <p class="font-bold flex items-start w-full overflow-hidden text-ellipsis h-[50px]">
+                ${product.name}
+              </p>
 
-            <div class="text-md flex items-center">
-              <svg class="mr-1" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="18px" fill="#EAC452">
-               <path d="M236.5-125 301-402 86-588l283.5-24.5 110.5-261 110.5 261L874-588 659-402l64.5 277L480-272 236.5-125Z"/>
-              </svg>
-              <div>
-                ${String(product.rating).replace('.', ',')} <span class="text-xs align-top text-gray-500">(${product.sold_quantity})</span>
+              <div class="text-md flex items-center">
+                <div>
+                  ${product.rating.toFixed(1).replace('.', ',')}
+                </div>
+                <div class="flex mx-1">
+                  ${renderStarsByRating(product.rating)}
+                </div>
+                <div>
+                  <span class="text-xs align-top text-gray-500">(${product.sold_quantity})</span>
+                </div>
               </div>
+
+              <p class="text-2xl flex items-center my-2">
+                <span class="text-sm align-text-top">R$</span>${value}<span class="text-sm align-text-top">${cents}</span>
+              </p>
+
+              <div class="w-full flex">
+                <button class="text-sm rounded-2xl self-center py-1 w-full mx-2 bg-orange-300 hover:cursor-pointer" onclick="addToCart(${product.id})">Adicionar ao carrinho</button>
+              </div>
+
+              <p class="text-xs text-gray-500 font-bold mt-2 ${!shoppingCartMap[product.id] ? 'hidden' : ''}">
+                ${shoppingCartMap[product.id]?.amount > 9 ? '9+' : shoppingCartMap[product.id]?.amount} no carrinho - <button class="underline hover:cursor-pointer" onclick="removeFromCart(${product.id})">excluir</button>
+              </p>
             </div>
-
-            <p class="text-2xl flex items-center my-2">
-              <span class="text-sm align-text-top">R$</span>${value}<span class="text-sm align-text-top">${cents}</span>
-            </p>
-
-            <div class="w-full flex">
-              <button class="text-sm rounded-2xl self-center py-1 w-full mx-2 bg-orange-300 hover:cursor-pointer" onclick="addToCart(${product.id})">Adicionar ao carrinho</button>
-            </div>
-
-            <p class="text-xs text-gray-500 font-bold mt-2 ${!shoppingCartMap[product.id] ? 'hidden' : ''}">
-              ${shoppingCartMap[product.id]?.amount > 9 ? '9+' : shoppingCartMap[product.id]?.amount} no carrinho - <button class="underline hover:cursor-pointer" onclick="removeFromCart(${product.id})">excluir</button>
-            </p>
           </div>
         </div>
-      </div>
-    `;
+      `;
     }).join('');
+}
+
+function renderStarsByRating(rating) {
+  const filledStarSvg = `<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#EAC452">
+      <path d="M236.5-125 301-402 86-588l283.5-24.5 110.5-261 110.5 261L874-588 659-402l64.5 277L480-272 236.5-125Z"/>
+    </svg>`;
+  const semiFilledStarSvg = `<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#EAC452">
+      <path d="m606-286-33-144 111-96-146-13-58-136v312l126 77ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"/>
+    </svg>`;
+  const emptyStarSvg = `<svg xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 -960 960 960" width="18px" fill="#EAC452">
+      <path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z"/>
+    </svg>`;
+
+  const filledStars = parseInt(rating);
+  const semiFilledStars = (rating - filledStars) >= 0.5 ? 1 : 0;
+  const emptyStars = parseInt(5 - filledStars - semiFilledStars);
+
+  return [
+    ...Array(filledStars).fill(filledStarSvg),
+    ...Array(semiFilledStars).fill(semiFilledStarSvg),
+    ...Array(emptyStars).fill(emptyStarSvg)
+  ].join('');
 }
 
 function updateCartCounter() {
